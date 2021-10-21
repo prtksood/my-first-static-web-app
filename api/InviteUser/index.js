0e128d5a30ed467538d609b575a3775d32528dd3
Prateek Sood <prateek.sood88@gmail.com>
2021-10-22 01:42:55 +0530

    removed test log

--- api/InviteUser/index.js
+++ api/InviteUser/index.js
@@ -28,7 +28,6 @@ const auth = require('./auth');
 //     // };
 // };
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
     context.res.json({
         text: "Hello from the API"
     });
