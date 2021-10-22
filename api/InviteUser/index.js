5359d9e41556bbe79e7ac9ba4901a42038ceca9f
Prateek Sood <prateek.sood88@gmail.com>
2021-10-22 19:09:23 +0530

    fixed may be

--- api/InviteUser/index.js
+++ api/InviteUser/index.js
@@ -1,36 +1,41 @@
-// module.exports = async function (context, req) {
-//     context.log('JavaScript HTTP trigger function processed a request.');
-
-//     const name = (req.query.name || (req.body && req.body.name));
-//     const responseMessage = name
-//         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-//         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-//     context.res = {
-//         // status: 200, /* Defaults to 200 */
-//         body: responseMessage
-//     };
-// }
-
-
-//require('dotenv').config();
-const fetch = require('./fetch');
-//const auth = require('./auth');
-// module.exports = async function (context, req) {
-//     context.log('JavaScript HTTP trigger function processed a request.');
-//     const authResponse = await auth.getToken(auth.tokenRequest);
-//     context.log(authResponse);
-//     const guestInvite = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
-//     context.log(guestInvite);
-//     context.res.json({
-//         inviteID: guestInvite.id
-//     });
-//     // context.res = {
-//     // body: guestInvite
-//     // };
-// };
+require('dotenv').config();
+const msal = require('@azure/msal-node');
+const axios = require('axios');
 module.exports = async function (context, req) {
+    const msalConfig = {
+        auth: {
+            clientId: "afe50edf-6d86-42dd-95a8-b443393beaf4",
+            authority: "https://login.microsoftonline.com/89d0242e-efe6-4db7-a750-f8adfe340445",
+            clientSecret: "IL17Q~ZLJc.a1m12fdCt6MQazR5FlvXz4Nojd",
+        }
+    };
+    const tokenRequest = {
+        scopes: ['https://graph.microsoft.com/.default'],
+    };
+    const apiConfig = {
+        // uri: process.env.GRAPH_ENDPOINT + 'v1.0/users',
+        uri: 'https://graph.microsoft.com/v1.0/invitations',
+    };
+    const cca = new msal.ConfidentialClientApplication(msalConfig);
+    const authResponse = await cca.acquireTokenByClientCredential(tokenRequest);
+
+    const options = {
+        headers: {
+            Authorization: `Bearer ${authResponse.accessToken}`
+        }
+    };
+
+    const invitation = {
+        invitedUserEmailAddress: 'prtksood@yahoo.co.in',
+        inviteRedirectUrl: 'https://casestudydev.sharepoint.com/sites/TrainingPortal',
+        sendInvitationMessage:true,
+        invitedUserMessageInfo: {
+            customizedMessageBody: 'Invited By Graph API'
+        }
+      };
+    const response = await axios.default.post(apiConfig.uri, invitation, options);
+    const guestInvite =  response.data;
     context.res.json({
-        text: "Hello from the API"
+        inviteID: guestInvite.id
     });
 };
